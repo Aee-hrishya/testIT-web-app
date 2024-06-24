0e0a5e6ecd7abaa9c2e37b1f125f1603e756656b
hrishikesh <hshinde166@gmail.com>
2024-06-25 02:20:12 +0530

    -imported typeDefs, resolvers for setting up apollo server

--- server/server.js
+++ server/server.js
@@ -3,21 +3,27 @@ const cors = require("cors");
 const mongoose = require("./db");
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server-express");
+const typeDefs = require("./graphql/schema");
+const resolvers = require("./graphql/resolvers");
 
-const app = express();
-app.use(cors());
+const startServer = async () => {
+  const app = express();
+  app.use(cors());
 
-//setting up apollo server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+  //setting up apollo server
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-await server.start();
-server.applyMiddleware({ app });
+  await server.start();
+  server.applyMiddleware({ app });
 
-//port started on the below port number
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  //port started on the below port number
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer();

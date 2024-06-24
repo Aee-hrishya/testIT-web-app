a5272e235cb3c8afb9b5d4fb1b075aa9a1c29e7d
hrishikesh <hshinde166@gmail.com>
2024-06-25 02:19:38 +0530

    -removed deprecated value

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 const { MONGO_URI } = process.env;
 
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));

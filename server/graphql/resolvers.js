9c21b5bd20f2e68c55718ef763ddbad3f96e7a29
hrishikesh <hshinde166@gmail.com>
2024-06-27 02:20:50 +0530

    -code for createUser resolver

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,7 +1,54 @@
+const { User } = require("../models/User");
+const { bcrypt } = require("bcrypt");
+
 const resolvers = {
   Mutation: {
-    createUser: (_, { username, password }) => {
-      console.log("hey");
+    createUser: async (_, { username, password }) => {
+      try {
+        //Validate password field
+        if (!password || password.trim === "") {
+          return {
+            success: false,
+            error: "Password is required.",
+          };
+        }
+
+        const saltRounds = 10;
+        //hash password for safety reasons
+        const hashedPassword = bcrypt.hash(password, saltRounds);
+        const newUser = new User({
+          username,
+          password: hashedPassword,
+        });
+
+        await newUser.save();
+
+        return {
+          success: true,
+          newUser,
+        };
+      } catch (error) {
+        //we are using default mongodb error handling here as our User schema has username field set to "unique:true"
+        if (error.code == 11000) {
+          return {
+            success: false,
+            error: "Username already exists.",
+          };
+        }
+
+        //We are using the default mongodb error handling here as our username field has 2 options minLength and maxLength
+        if (error.name === "validationError") {
+          return {
+            success: false,
+            error: error.message,
+          };
+        }
+
+        return {
+          success: false,
+          error: error.message,
+        };
+      }
     },
   },
 };

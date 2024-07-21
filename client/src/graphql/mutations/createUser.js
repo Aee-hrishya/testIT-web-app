f0705676f31a16c447005ed66e79f6f74a872592
hrishikesh <hshinde166@gmail.com>
2024-07-21 20:15:41 +0530

    -added role parameter to create and login user mutations

--- client/src/graphql/mutations/createUser.js
+++ client/src/graphql/mutations/createUser.js
@@ -1,14 +1,16 @@
 import { gql } from "@apollo/client";
 
 const CREATE_USER = gql`
-  mutation CreateUser($username: String!, $password: String!) {
-    createUser(username: $username, password: $password) {
+  mutation CreateUser($username: String!, $password: String!, $role: String!) {
+    createUser(username: $username, password: $password, role: $role) {
       success
       error
       user {
         id
         username
+        role
       }
+      token
     }
   }
 `;
--- client/src/graphql/mutations/loginUser.js
+++ client/src/graphql/mutations/loginUser.js
@@ -8,7 +8,9 @@ const LOGIN_USER = gql`
       user {
         id
         username
+        role
       }
+      token
     }
   }
 `;

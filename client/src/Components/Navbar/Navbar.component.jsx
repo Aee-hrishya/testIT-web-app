0bef603cc93e926b9c429a71886711a4c114c410
hrishikesh <hshinde166@gmail.com>
2024-07-26 03:10:53 +0530

    -added logout functionality and logic for page reloads to navbar

--- client/src/Components/Navbar/Navbar.component.jsx
+++ client/src/Components/Navbar/Navbar.component.jsx
@@ -1,8 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { clearUser, setUser } from "../../Redux/Slices/userSlice";
 
 const Navbar = () => {
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logoutHandler = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("authToken");
+    dispatch(clearUser());
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      dispatch(setUser({ token: token }));
+    } else {
+      console.log("Token is not present.");
+      navigate("/login");
+    }
+  }, [dispatch, navigate]);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, navigate]);
+
   return (
     <>
       <div className="nav-bar">
@@ -11,14 +40,20 @@ const Navbar = () => {
             Test It
           </Link>
         </div>
-        <div className="action-section">
-          <Link className="about" to="/about">
-            About Us
+        {isAuthenticated ? (
+          <Link className="authenticated-navbar" onClick={logoutHandler}>
+            Logout
           </Link>
-          <Link className="login" to="/login">
-            Login/Signup
-          </Link>
-        </div>
+        ) : (
+          <div className="action-section">
+            <Link className="about" to="/about">
+              About Us
+            </Link>
+            <Link className="login" to="/login">
+              Login/Signup
+            </Link>
+          </div>
+        )}
       </div>
     </>
   );

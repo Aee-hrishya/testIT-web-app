a3b769746a9be68ec126596b6922edc67db54ce3
hrishikesh <hshinde166@gmail.com>
2024-07-26 03:12:25 +0530

    -added role field to user state and added the clearUser reducer

--- client/src/Redux/Slices/userSlice.js
+++ client/src/Redux/Slices/userSlice.js
@@ -7,17 +7,28 @@ const userSlice = createSlice({
     user: {
       id: null,
       username: null,
+      role: null,
     },
   },
   reducers: {
     setUser(state, action) {
-      state.isAuthenticated = action.payload.token;
-      state.user.id = action.payload.user.id;
-      state.user.username = action.payload.user.username;
+      state.isAuthenticated = action.payload.token ?? state.isAuthenticated;
+      state.user.id = action.payload.user?.id ?? state.user.id;
+      state.user.username =
+        action.payload.user?.username ?? state.user.username;
+      state.user.role = action.payload.user?.role ?? state.user.role;
+    },
+    clearUser(state) {
+      state.isAuthenticated = null;
+      state.user = {
+        id: null,
+        username: null,
+        role: null,
+      };
     },
   },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, clearUser } = userSlice.actions;
 
 export default userSlice.reducer;

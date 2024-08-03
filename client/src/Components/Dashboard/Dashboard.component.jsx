d74e6118abb7ca6a65fa3b696031f544290a9cc5
hrishikesh <hshinde166@gmail.com>
2024-08-04 01:28:17 +0530

    -added code for handling loading states and minor changes in state updates

--- client/src/Components/Dashboard/Dashboard.component.jsx
+++ client/src/Components/Dashboard/Dashboard.component.jsx
@@ -1,37 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Dashboard.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "../../Redux/Slices/userSlice";
 import GET_DASHBOARD_DETAILS from "../../graphql/queries/getDashboardDetails";
 import { useQuery } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
+import CircleLoader from "react-spinners/CircleLoader";
 
 const Dashboard = () => {
-  const token = useSelector((state) => state.user.isAuthenticated);
+  const [token, setToken] = useState(localStorage.getItem("authToken"));
+  const [loader, setLoader] = useState(false);
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const tokenInLocalStorage = localStorage.getItem("authToken");
+    if (!tokenInLocalStorage) {
+      // Redirect to login if token is not found in local storage
+      setLoader(true);
+      navigate("/login");
+    } else {
+      setToken(tokenInLocalStorage);
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    // Check token validity and redirect if necessary
+    if (!token) {
+      setLoader(true);
+      navigate("/login");
+    }
+  }, [token, navigate]);
+
   const { data, loading, error } = useQuery(GET_DASHBOARD_DETAILS, {
     skip: !token, //kindly skip the api call if token is not present
   });
   const { success, error: dashboardError, userDetails } = data?.dashboard || {};
-  const user = useSelector((state) => state.user.user);
 
   useEffect(() => {
     if (success && userDetails) {
+      // Update user data in Redux store if authentication is successful
       dispatch(
         setUser({
           user: userDetails,
         })
       );
     }
-  }, [success, userDetails, dispatch]);
+  }, [userDetails, success, dispatch]);
 
-  if (!token) {
-    return navigate("/login");
-  }
-
-  if (loading) {
-    return <div>Loading...</div>;
+  if (!token || loading || loader) {
+    return (
+      <div className="loader">
+        <CircleLoader size={80} />
+      </div>
+    );
   }
 
   if (error || dashboardError) {

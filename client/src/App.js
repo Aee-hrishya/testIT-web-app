b9bdf5b64e37bab9ce4905ac144d3e95d09e0dbc
hrishikesh <hshinde166@gmail.com>
2024-08-04 01:26:52 +0530

    -useEffect to set token in redux state if present in localStorage

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,22 @@ import Navbar from "./Components/Navbar/Navbar.component";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute.component";
 import Dashboard from "./Components/Dashboard/Dashboard.component";
+import { useDispatch } from "react-redux";
+import { setUser } from "./Redux/Slices/userSlice";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      dispatch(
+        setUser({
+          token: token,
+        })
+      );
+    }
+  }, [dispatch]);
   return (
     <div className="App">
       <BrowserRouter>
